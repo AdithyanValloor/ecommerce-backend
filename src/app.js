fe9d3f09351617567f47a83f41f7dd570470def9
AdithyanValloor <165929678+AdithyanValloor@users.noreply.github.com>
2025-02-13 21:00:02 +0530

    errors fixed

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json());
 app.use(cors(
     {
-    origin:['http://localhost:5174', 'https://chimerical-travesseiro-acfb77.netlify.app']
+    origin:['https://chimerical-travesseiro-acfb77.netlify.app', 'http://localhost:5174']
     }
 ));
 
